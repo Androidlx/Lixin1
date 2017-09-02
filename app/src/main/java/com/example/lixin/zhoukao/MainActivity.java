9eebf29dac7698f3e879600dee2d0bd6ddf0de87
Androidlx <1748054721@qq.com>
2017-09-02 09:31:17 +0800

    修改完毕

--- app/src/main/java/com/example/lixin/zhoukao/MainActivity.java
+++ app/src/main/java/com/example/lixin/zhoukao/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.lixin.zhoukao;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyCustomView.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyCustomView customView = (MyCustomView) findViewById(R.id.customview);
+        customView.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void leftonClickListener(View left_btn) {
+        Toast.makeText(this, "点击了 返回", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void RightClickListener(View right_btn) {
+        Toast.makeText(this, "点击了 更多", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/lixin/zhoukao/MyCustomView.java
@@ -0,0 +1,137 @@
+package com.example.lixin.zhoukao;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by hua on 2017/9/2.
+ */
+
+public class MyCustomView extends LinearLayout {
+
+
+    private Button left_btn;
+    private TextView center_text;
+    private Button right_btn;
+    private int left_btn_text_color;
+    private String left_btn_text;
+    private float left_btn_size;
+    private int left_btn_bg;
+    private String center_text1;
+    private int center_text_color;
+    private float center_text_size;
+    private int center_bg;
+    private String right_btn_text;
+    private int right_btn_text_color;
+    private float right_btn_size;
+    private int right_btn_bg;
+
+    public MyCustomView(Context context) {
+        super(context);
+        initView(context,null);
+    }
+
+
+
+    public MyCustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initView(context,attrs);
+    }
+    private void initView(Context context,@Nullable AttributeSet attrs) {
+
+        View inflate = inflate(context, R.layout.custom_view, this);
+        left_btn = inflate.findViewById(R.id.left_btn);
+        center_text = inflate.findViewById(R.id.center_text);
+        right_btn = inflate.findViewById(R.id.right_btn);
+
+        left_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOnClickListener!=null){
+                    mOnClickListener.leftonClickListener(view);
+                }
+            }
+        });
+
+        right_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOnClickListener!=null){
+                    mOnClickListener.RightClickListener(view);
+                }
+            }
+        });
+
+
+
+        if (attrs==null){
+            return;
+        }
+
+        initAttrs(context,attrs);
+        setViewContext();
+    }
+    private void initAttrs(Context context,@Nullable AttributeSet attrs) {
+
+        if (attrs==null){
+            return;
+        }
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MyCustomView);
+
+        left_btn_text = typedArray.getString(R.styleable.MyCustomView_left_btn_text);
+        left_btn_text_color = typedArray.getColor(R.styleable.MyCustomView_left_btn_text_color, 0xff0000);
+        left_btn_size = typedArray.getDimension(R.styleable.MyCustomView_left_btn_text_size, 18);
+        left_btn_bg = typedArray.getColor(R.styleable.MyCustomView_left_btn_bg, 0x00ff00);
+
+        center_text1 = typedArray.getString(R.styleable.MyCustomView_center_text);
+        center_text_color = typedArray.getColor(R.styleable.MyCustomView_center_text_color,0xff0000);
+        center_text_size = typedArray.getDimension(R.styleable.MyCustomView_center_text_size,18);
+        center_bg = typedArray.getColor(R.styleable.MyCustomView_center_text_bg,0x00ff00);
+
+        right_btn_text = typedArray.getString(R.styleable.MyCustomView_right_btn_text);
+        right_btn_text_color = typedArray.getColor(R.styleable.MyCustomView_right_btn_text_color,0xff0000);
+        right_btn_size = typedArray.getDimension(R.styleable.MyCustomView_right_btn_text_size,18);
+        right_btn_bg = typedArray.getColor(R.styleable.MyCustomView_right_btn_bg,0x00ff00);
+
+    }
+    private void setViewContext() {
+        left_btn.setText(left_btn_text);
+        left_btn.setTextColor(left_btn_text_color);
+        left_btn.setBackgroundColor(left_btn_bg);
+        left_btn.setTextSize(left_btn_size);
+
+
+        center_text.setText(center_text1);
+        center_text.setTextColor(center_text_color);
+        center_text.setBackgroundColor(center_bg);
+        center_text.setTextSize(center_text_size);
+
+
+        right_btn.setText(right_btn_text);
+        right_btn.setTextColor(right_btn_text_color);
+        right_btn.setBackgroundColor(right_btn_bg);
+        right_btn.setTextSize(right_btn_size);
+
+    }
+
+    public interface OnClickListener{
+        void leftonClickListener(View left_btn);
+        void RightClickListener(View right_btn);
+    }
+    private OnClickListener mOnClickListener;
+
+    public void setOnClickListener(OnClickListener onClickListener){
+        mOnClickListener = onClickListener;
+    }
+
+
+
+
+}
+
